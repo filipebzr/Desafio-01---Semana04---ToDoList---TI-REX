381e10957b74fc8ef69fca77b2343307c06c92d1
FilipeBzr <fil@pe.com>
2024-12-11 17:41:36 -0300

    login page is coming

---
+++ Project1/script.js
@@ -0,0 +1,12 @@
+document.getElementById('linkedin').addEventListener('click', function () {
+    window.location.href = 'https://www.linkedin.com';
+});
+document.getElementById('instagram').addEventListener('click', function () {
+    window.location.href = 'https://www.instagram.com';
+});
+document.getElementById('facebook').addEventListener('click', function () {
+    window.location.href = 'https://www.facebook.com';
+});
+
+
+
